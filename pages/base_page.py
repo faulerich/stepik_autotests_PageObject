c3d29113f8fdc677d5715d56c83a0d43a66178d1
faulerich <faulerich@yandex.ru>
2024-02-22 16:20:29 +0300

    4.2.6: updated assert in method "should_be_login_link" to main_page.py added "is_element_present", updated __init__ to base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,12 +3,23 @@
 # базовая страница, от которой будут унаследованы все остальные классы.
 # в ней мы опишем вспомогательные методы для работы с драйвером.
 
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage():
-    # конструктор (метод, который вызывается, когда мы создаем объект)
-    def __init__(self, browser, url):
+    # конструктор (метод, который вызывается, когда мы создаем объект) с неявным ожиданием в 10 сек
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     # метод "open" должен открывать нужную страницу в браузере, используя метод get()
     def open(self):
         self.browser.get(self.url)
+
+    # метод "is_element_present" будет перехватывать исключения (how - как искать, what - что искать (селектор))
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
--- pages/main_page.py
+++ pages/main_page.py
@@ -11,7 +11,7 @@ class MainPage(BasePage):
 
     # метод проверки наличия ссылки, которая ведет на форму логина
     def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
 
     # метод нажатия ссылки логина (переход на страницу логина)
     def go_to_login_page(self):
