6b0a5b23c32029d2ca8ae6687d272d88fb89f6e6
faulerich <faulerich@yandex.ru>
2024-02-26 10:48:51 +0300

    4.2.7: added locators.py updated main_page.py for using locators.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,6 +3,7 @@
 
 # импорт базового класса BasePage
 from .base_page import BasePage
+from .locators import MainPageLocators
 from selenium.webdriver.common.by import By
 
 # класс MainPage - наследник класса BasePage
@@ -11,9 +12,10 @@ class MainPage(BasePage):
 
     # метод проверки наличия ссылки, которая ведет на форму логина
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+        # * указывает на то, что передаем пару (тип локатора и его значение) и ее надо распаковать
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
 
     # метод нажатия ссылки логина (переход на страницу логина)
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
