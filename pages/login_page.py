2d959fd6559d0e3cca51fe1f085b3aa2d10f15c5
faulerich <faulerich@yandex.ru>
2024-02-26 12:37:43 +0300

    4.2.8: updated locators.py added login_page.py (PageObject for login page)

---
+++ pages/login_page.py
@@ -0,0 +1,24 @@
+
+# Page Object для страницы логина (4.2.8)
+
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        # в данном случае проверяем что подстрока "login" есть в текущем url браузера
+        assert "login" in self.current_url, "Incorrect login url"
+
+    def should_be_login_form(self):
+        # проверка, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        # проверка, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -3,6 +3,7 @@
 
 from selenium.webdriver.common.by import By
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
