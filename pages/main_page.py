e7460e073bfbd384ec56c7fb03f6498590cac1f8
faulerich <faulerich@yandex.ru>
2024-02-26 13:17:28 +0300

    4.2.9: updated test_main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -4,6 +4,7 @@
 # импорт базового класса BasePage
 from .base_page import BasePage
 from .locators import MainPageLocators
+from .login_page import LoginPage
 from selenium.webdriver.common.by import By
 
 # класс MainPage - наследник класса BasePage
